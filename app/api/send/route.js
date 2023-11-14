8dccf11fa78ba10b4689752ed144943b90d86955
Aaron Saralegui <aaron.saralegui@gmail.com>
2023-11-13 17:52:45 -0700

    fixing email section

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -6,8 +6,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
-  const { body } = req;
-  const { email, subject, messege } = body;
+  const { body } = await req.json();
+  const { email, subject, message } = body;
   try {
     const data = await resend.emails.send({
       from: fromEmail,
